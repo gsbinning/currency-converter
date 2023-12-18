336a4cdc67d665ab9a1c378f52f890d03836db4e
gsbinning <gugandeep.s@hotmail.com>
2023-12-18 14:36:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+function updateRate() {
+  console.log("Called");
+}
+
+currencyFirstEl.addEventListener("change", updateRate);
+currencySecondEl.addEventListener("change", updateRate);
+worthFirstEl.addEventListener("input", updateRate);
